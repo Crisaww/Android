94e975180fcd84feed75aa194b71a0283e8823d6
unknown <mariajosemurciamartinez85@gmail.com>
2024-07-12 23:29:02 -0500

    Se actualiza

--- EjerciciosAndroid/mockupTaller/mockupTaller/app/src/main/java/com/example/mockuptaller/inicio_pagina.kt
+++ EjerciciosAndroid/mockupTaller/mockupTaller/app/src/main/java/com/example/mockuptaller/inicio_pagina.kt
@@ -1,5 +1,6 @@
 package com.example.mockuptaller
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -16,6 +17,5 @@ class inicio_pagina: AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
     }
 }
