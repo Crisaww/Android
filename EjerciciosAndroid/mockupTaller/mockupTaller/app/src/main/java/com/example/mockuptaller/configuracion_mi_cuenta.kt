dba159a203b5f6c601c8fd762aa60b78cea04ea0
Carolina <carolina22cmc@gmail.com>
2024-07-16 23:41:13 -0500

    se modifico la mayoria

--- EjerciciosAndroid/mockupTaller/mockupTaller/app/src/main/java/com/example/mockuptaller/configuracion_mi_cuenta.kt
+++ EjerciciosAndroid/mockupTaller/mockupTaller/app/src/main/java/com/example/mockuptaller/configuracion_mi_cuenta.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -19,6 +20,14 @@ class configuracion_mi_cuenta : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        var btnVolver = findViewById<ImageView>(R.id.btnVolver)
+        btnVolver.setOnClickListener {
+            /*
+            finish elimina o cierra la activity actual
+             */
+            finish()
+
         }
     }
 }
