06abc7d1a9ad602467749674e73f3f1f3dfc8595
Carolina <carolina22cmc@gmail.com>
2024-07-17 15:37:36 -0500

    se actualizaron todos los iconos

--- EjerciciosAndroid/mockupTaller/mockupTaller/app/src/main/java/com/example/mockuptaller/configuracion_mi_cuenta.kt
+++ EjerciciosAndroid/mockupTaller/mockupTaller/app/src/main/java/com/example/mockuptaller/configuracion_mi_cuenta.kt
@@ -1,5 +1,7 @@
 package com.example.mockuptaller
 
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -30,4 +32,5 @@ class configuracion_mi_cuenta : AppCompatActivity() {
 
         }
     }
+
 }
