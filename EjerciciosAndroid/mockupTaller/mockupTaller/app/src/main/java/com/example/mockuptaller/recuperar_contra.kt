7707f2c1e0ea02b049b11ab56a0be8a6283239a8
Carolina <carolina22cmc@gmail.com>
2024-07-18 16:51:42 -0500

    se modifico

--- EjerciciosAndroid/mockupTaller/mockupTaller/app/src/main/java/com/example/mockuptaller/recuperar_contra.kt
+++ EjerciciosAndroid/mockupTaller/mockupTaller/app/src/main/java/com/example/mockuptaller/recuperar_contra.kt
@@ -1,10 +1,14 @@
 package com.example.mockuptaller
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
 
 class recuperar_contra: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +20,21 @@ class recuperar_contra: AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val btnEnviar: Button = findViewById(R.id.btnRecuperarContra)
+        btnEnviar.setOnClickListener {
+            mostrarAlerta()
+        }
     }
+        fun mostrarAlerta() {
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle("Correo Enviado")
+            builder.setMessage("Por favor revise su bandeja de entrada.")
+            builder.setPositiveButton("OK") { dialog, _ ->
+                // Acción al hacer clic en OK
+                dialog.dismiss()
+            }
+
+            val alerta = builder.create()
+            alerta.show()
+        }
 }
