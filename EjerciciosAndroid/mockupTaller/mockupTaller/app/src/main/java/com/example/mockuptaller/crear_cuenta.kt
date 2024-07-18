a1780d200e22b33cd82638bd437b085f743c99c9
Carolina <carolina22cmc@gmail.com>
2024-07-18 17:18:37 -0500

    se coloco el boton de volver

--- EjerciciosAndroid/mockupTaller/mockupTaller/app/src/main/java/com/example/mockuptaller/crear_cuenta.kt
+++ EjerciciosAndroid/mockupTaller/mockupTaller/app/src/main/java/com/example/mockuptaller/crear_cuenta.kt
@@ -3,6 +3,7 @@ package com.example.mockuptaller
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -17,6 +18,13 @@ class crear_cuenta: AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        var btnVolver = findViewById<ImageView>(R.id.btnVolver)
+        btnVolver.setOnClickListener {
+            /*
+            finish elimina o cierra la activity actual
+             */
+            finish()
         }
     }
 
